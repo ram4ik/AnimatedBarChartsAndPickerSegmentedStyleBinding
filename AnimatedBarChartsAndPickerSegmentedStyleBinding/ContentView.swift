1663263baddef53bef14edae26b6670ecabd9e61
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-04 10:00:43 +0300

    tagged pickerview

--- AnimatedBarChartsAndPickerSegmentedStyleBinding/ContentView.swift
+++ AnimatedBarChartsAndPickerSegmentedStyleBinding/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var pickerSelectedItem = 0
+    
     var body: some View {
-        Text("Hello World")
+        ZStack {
+           
+            Color(#colorLiteral(red: 0.5843137503, green: 0.8235294223, blue: 0.4196078479, alpha: 1)).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Calory Intake")
+                    .font(.system(size: 34))
+                    .fontWeight(.heavy)
+                
+                Picker(selection: $pickerSelectedItem, label: Text("")) {
+                    Text("Weekday").tag(0)
+                    Text("Afternoon").tag(1)
+                    Text("Evening").tag(2)
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+        }
     }
 }
 
